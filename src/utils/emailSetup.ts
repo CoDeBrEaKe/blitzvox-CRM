20a292a4de0f0c021ae03b40b0b95f2762abed21
Mohamed's-hub <85935508+JSCODER1-hub@users.noreply.github.com>
2025-10-20 07:06:03 +0300

    added .env to tsconfig

--- src/utils/emailSetup.ts
+++ src/utils/emailSetup.ts
@@ -2,6 +2,8 @@ import nodemailer from "nodemailer";
 import * as dotenv from "dotenv";
 
 dotenv.config();
+console.log(process.env.EMAIL_USER);
+console.log(process.env.EMAIL_PASS);
 // transporter config (example: Gmail SMTP)
 export const transporter = nodemailer.createTransport({
   service: "gmail", // or use `host`, `port`, `secure` for custom SMTP
